16645920c8a050df0036baff6ec4cb4ce3a6226c
drak@kaverne <drak@kaverne>
2015-03-16 12:04:28 +0100

    fix localization and other minor stuff

--- src/build.sh
+++ src/build.sh
@@ -3,8 +3,8 @@
 # It is assumed the Java Compiler from OpenJDK 7 is used.
 # The produced jar file will run on both Java 6 and Java 7.
 
-rm plugins/ShareLink/*.class plugins/ShareLink/*/*.class
-rm ShareLink*.jar
+rm plugins/ShareWiki/*.class plugins/ShareWiki/*/*.class
+rm ShareWiki*.jar
 
-javac -cp ../freenet-ext.jar:../freenet.jar -source 1.6 -target 1.6 plugins/ShareLink/*.java plugins/ShareLink/*/*.java
-zip -r ShareLink.jar *
+javac -cp ../freenet-ext.jar:../freenet.jar -source 1.6 -target 1.6 plugins/ShareWiki/*.java plugins/ShareWiki/*/*.java
+zip -r ShareWiki.jar *
