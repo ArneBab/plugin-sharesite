7d90c5aed954f0f3a5db53ba763950e48d7477b4
drak@kaverne <drak@kaverne>
2017-02-07 14:41:25 +0100

    update to 0.4.2

--- src/plugins/Sharesite/Plugin.java
+++ src/plugins/Sharesite/Plugin.java
@@ -21,7 +21,7 @@ import java.awt.GraphicsEnvironment;
  * the global variable. The most logical way to reach different things.
  */
 public class Plugin implements FredPlugin, FredPluginVersioned, FredPluginRealVersioned, FredPluginL10n, FredPluginBaseL10n, FredPluginThreadless {
-	private static final String version = "0.4.1";
+	private static final String version = "0.4.2";
 	public static final long realVersion = 3;
 	public static final String freesite = "USK@dCnkUL22fAmKbKg-Cftx9j2m4IwyWB0QbGoiq1RSLP8,4d1TDqwRr4tYlsubLrQK~c4h0~FtmE-OXCDmFiI8BB4,AQACAAE/Sharesite/-18/";
 	public static boolean isPreRelease = false;
